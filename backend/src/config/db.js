c416743b5825257d19479ec099181e4d1b0b1fdd
TuIT <congtuu198@gmail.com>
2026-05-28 17:08:42 +0700

    First commit

---
+++ backend/src/config/db.js
@@ -0,0 +1,14 @@
+const { Sequelize } = require("sequelize");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const sequelize = new Sequelize(
+  process.env.DATABASE_URL ||
+    "postgres://postgres:postgres@localhost:5432/ecommerce_db",
+  {
+    dialect: "postgres",
+    logging: false,
+  },
+);
+
+module.exports = sequelize;
